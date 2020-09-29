7d3b128c08db4d2780c9d67167a3c7cddf1ff13d
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-29 00:43:10 -0600

    2.5-7 createGlobalStyle and Conditionals based on props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import { createGlobalStyle } from 'styled-components';
+import { Button } from 'components/common';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height:100vh;
+    margin: 0;
+    color: black;
+    font-family:'Kaushan Script', cursive
+  }
+`;
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/common/Button.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  ${(props) => {
+    if (props.secondary) {
+      return { color: 'white', background: 'black' };
+    } else {
+      return { color: 'black', background: '#f8049c' };
+    }
+  }}
+  font-weight: bold;
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: none;
+  font-size: 1em;
+  border: none;
+  width: 100%;
+  display: block;
+  white-space: none;
+
+  &:disabled {
+    background: #eee;
+    color: #666;
+  }
+`;
+
+export { Button };
