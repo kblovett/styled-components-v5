5457a15fc20a3c221b78881f3118f3fc3212e1e7
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-29 20:07:34 -0600

    3.16 Input styled component creation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,5 +3,17 @@ import React from 'react';
 import { PageLayout } from 'components/common';
 
 export default function Home() {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia vitae
+        quis porro soluta exercitationem inventore cum maxime ducimus veniam
+        enim accusantium optio sit excepturi esse aut quod nesciunt ullam, a ea
+        in nam, at error dignissimos quasi! Eum, quis delectus vel facere
+        distinctio accusantium repellendus rem doloremque totam blanditiis
+        fugiat?
+      </p>
+    </PageLayout>
+  );
 }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import { PageLayout } from 'components/common';
+import { PageLayout, Input, Button } from 'components/common';
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 export default function Login() {
-  return <PageLayout>Login</PageLayout>;
+  const [formFields, setFormFields] = useState({ username: '', password: '' });
+
+  function handleInputChange(event) {
+    event.persist();
+    setFormFields((s) => ({
+      ...s,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  return (
+    <PageLayout>
+      <h1>Login</h1>
+      <Form>
+        <Input
+          name='username'
+          placeholder='Username'
+          onChange={handleInputChange}
+          value={formFields.username}
+          type='text'
+        />
+        <Input
+          name='password'
+          placeholder='Password'
+          onChange={handleInputChange}
+          value={formFields.password}
+          type='password'
+        />
+        <Button>Submit</Button>
+        <Button secondary>Submit</Button>
+        <Button disabled>Submit</Button>
+        <Button size='large'>Submit</Button>
+      </Form>
+    </PageLayout>
+  );
 }
