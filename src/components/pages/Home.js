0ff87f1f8f0e67a77962e925423141465b31c443
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-30 20:29:33 -0600

    add more text to Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,6 +14,34 @@ export default function Home() {
         distinctio accusantium repellendus rem doloremque totam blanditiis
         fugiat?
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+        molestiae laborum reprehenderit, voluptate exercitationem consequatur
+        soluta illo ad amet beatae saepe recusandae eos quis ratione sit, nobis
+        fugit deleniti explicabo placeat, numquam ipsam natus commodi quo.
+        Assumenda labore a obcaecati.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
+        repellat optio nemo eum, animi quasi eveniet aspernatur molestiae
+        temporibus debitis rerum nihil tempora alias laudantium, aperiam dolores
+        dolor, explicabo ipsam. Non, quaerat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quasi
+        deleniti dolores accusantium pariatur! Aut maiores eveniet at quae odit?
+        Saepe amet maiores, tempora nihil corrupti dolores ex hic perferendis
+        quia doloremque, quidem eveniet. Omnis eveniet recusandae, libero enim,
+        assumenda facilis aut eligendi similique impedit dicta pariatur
+        deserunt. Nihil quisquam placeat consectetur nisi ex, eaque quas, soluta
+        nam incidunt pariatur aliquid dignissimos. Dolorum delectus magnam nisi
+        alias enim quod laboriosam culpa ea eum sed?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+        impedit, expedita, aliquid culpa assumenda hic eum amet natus porro esse
+        numquam labore ipsum, dicta sit odio!
+      </p>
     </PageLayout>
   );
 }
